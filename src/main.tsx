9669be71aa56a2596c2be08062b21825abae5dfc
AliHassan1617 <alihassan.ah1617@gmail.com>
2026-02-19 17:07:40 +0500

    Add Vercel Analytics and install @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GameProvider } from './state/gameContext'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GameProvider>
       <App />
+      {/* Vercel Analytics: counts page views and visitor metrics. */}
+      <Analytics />
     </GameProvider>
   </StrictMode>,
 )
